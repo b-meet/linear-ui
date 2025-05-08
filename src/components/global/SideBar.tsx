a9fa5c081d7f99f92689dbf68f122716e86d4c95
meet <meetbhalodiya1030@gmail.com>
2025-05-08 23:37:24 +0530

    chnage: nvaigation bar

--- src/components/global/SideBar.tsx
+++ src/components/global/SideBar.tsx
@@ -9,12 +9,19 @@ import {Link} from 'react-router';
 import {ROUTES} from '../../routing/routes';
 import {toast} from 'react-toastify';
 import {API_ROUTES} from '../../utility/constant';
+import GlobalModal from './GlobalModal';
+import {storageServices} from '../../utility/storageServices';
+import {STORAGE_SERVICES} from '../../type';
+import {useAppDispatch} from '../../hooks/redux';
+import {userActions} from '../../redux/slices/userSlice/userSlice';
 
 const SideBar = () => {
 	const [isOpen, setIsOpen] = useState(true);
 	const navigate = useNavigate();
 	const location = useLocation();
+	const dispatch = useAppDispatch();
 	const [addClaimId, setAddClaimId] = useState<string | null>(null);
+	const [showLogoutModal, setShowLogoutModal] = useState(false);
 
 	useEffect(() => {
 		if (addClaimId) {
@@ -36,54 +43,99 @@ const SideBar = () => {
 		}
 	};
 
+	const handleLogout = () => {
+		storageServices.terminate(STORAGE_SERVICES.LOCAL);
+		dispatch(userActions.resetUser());
+		navigate(ROUTES.LOGIN);
+	};
+
 	return (
-		<div
-			className={`h-full bg-white shadow-md py-4 px-2 flex flex-col transition-all duration-300 ${isOpen ? 'w-64' : 'w-16'}`}
-		>
-			<button
-				onClick={toggleSidebar}
-				className="text-gray-400 hover:text-gray-600 mb-6 max-w-max cursor-pointer"
+		<>
+			<div
+				className={`h-full bg-white shadow-md pb-4 px-2 flex flex-col transition-all duration-300 ${isOpen ? 'w-64' : 'w-16'}`}
 			>
-				{isOpen ? (
-					<GoSidebarExpand size={24} />
-				) : (
-					<GoSidebarCollapse size={24} />
-				)}
-			</button>
+				<div className="flex items-center gap-4 h-20">
+					<button
+						onClick={toggleSidebar}
+						className="text-gray-400 hover:text-gray-600 max-w-max cursor-pointer"
+					>
+						{isOpen ? (
+							<GoSidebarExpand size={24} />
+						) : (
+							<GoSidebarCollapse size={24} />
+						)}
+					</button>
+					{isOpen && (
+						<div className="text-xl font-medium">
+							<span className="text-brand font-bold">Linear</span>Claims
+						</div>
+					)}
+				</div>
 
-			<button
-				onClick={addClaim}
-				className="flex items-center gap-3 bg-brand hover:bg-brand-darker text-white text-sm px-4 py-3 rounded-md mb-4 cursor-pointer transition"
+				<button
+					onClick={addClaim}
+					className="flex items-center gap-3 bg-brand hover:bg-brand-darker text-white text-sm px-4 py-3 rounded-md mb-4 cursor-pointer transition"
+				>
+					<FaPlus />
+					{isOpen && <span className="whitespace-nowrap">New Claim</span>}
+				</button>
+				<div className="flex flex-col justify-between h-full">
+					<nav className="flex flex-col gap-1 text-sm">
+						<Link
+							to={'/dashboard'}
+							className="flex items-center gap-3 px-3 py-2 hover:bg-gray-100 rounded-md transition"
+						>
+							<MdOutlineSpaceDashboard className="text-xl" />
+							{isOpen && <span>Dashboard</span>}
+						</Link>
+						<Link
+							to={ROUTES.CLAIMS}
+							className="flex items-center gap-3 px-3 py-2 hover:bg-gray-100 rounded-md transition"
+						>
+							<MdOutlineLibraryBooks className="text-xl" />
+							{isOpen && <span>Claims</span>}
+						</Link>
+						<Link
+							to={'/customers'}
+							className="flex items-center gap-3 px-3 py-2 hover:bg-gray-100 rounded-md transition"
+						>
+							<FaRegUser className="text-xl" />
+							{isOpen && <span>Customers</span>}
+						</Link>
+					</nav>
+					<button
+						className="inline-block py-2 rounded-md bg-[#F9F9F9] hover:bg-slate-200 cursor-pointer"
+						onClick={() => setShowLogoutModal(true)}
+					>
+						Log out
+					</button>
+				</div>
+			</div>
+			<GlobalModal
+				isOpen={showLogoutModal}
+				onClose={() => setShowLogoutModal(false)}
+				title="Confirm Logout"
 			>
-				<FaPlus />
-				{isOpen && <span className="whitespace-nowrap">New Claim</span>}
-			</button>
-
-			{/* Nav Links */}
-			<nav className="flex flex-col gap-1 text-sm">
-				<Link
-					to={'/dashboard'}
-					className="flex items-center gap-3 px-3 py-2 hover:bg-gray-100 rounded-md transition"
-				>
-					<MdOutlineSpaceDashboard className="text-xl" />
-					{isOpen && <span>Dashboard</span>}
-				</Link>
-				<Link
-					to={ROUTES.CLAIMS}
-					className="flex items-center gap-3 px-3 py-2 hover:bg-gray-100 rounded-md transition"
-				>
-					<MdOutlineLibraryBooks className="text-xl" />
-					{isOpen && <span>Claims</span>}
-				</Link>
-				<Link
-					to={'/customers'}
-					className="flex items-center gap-3 px-3 py-2 hover:bg-gray-100 rounded-md transition"
-				>
-					<FaRegUser className="text-xl" />
-					{isOpen && <span>Customers</span>}
-				</Link>
-			</nav>
-		</div>
+				<p className="text-gray-600 mb-4">Are you sure you want to logout?</p>
+				<div className="flex justify-end gap-3">
+					<button
+						className="px-4.5 py-1.5 cursor-pointer rounded-md border border-gray-400 text-sm hover:bg-gray-200"
+						onClick={() => setShowLogoutModal(false)}
+					>
+						Cancel
+					</button>
+					<button
+						className="px-4.5 py-1.5 cursor-pointer rounded-md bg-red-500 text-white text-sm hover:bg-red-600"
+						onClick={() => {
+							handleLogout();
+							setShowLogoutModal(false);
+						}}
+					>
+						Logout
+					</button>
+				</div>
+			</GlobalModal>
+		</>
 	);
 };
 
--- src/pages/main/AppLayout.tsx
+++ src/pages/main/AppLayout.tsx
@@ -1,13 +1,11 @@
 import {Outlet} from 'react-router';
 import SideBar from '../../components/global/SideBar';
-import Navbar from '../../components/global/Navbar';
 
 const AppLayout = () => {
 	return (
 		<div className="flex h-screen">
 			<SideBar />
 			<section className="flex flex-col w-full">
-				<Navbar />
 				<div className="flex-1 p-4 bg-[#F9F9F9]">
 					<Outlet />
 				</div>
