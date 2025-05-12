10bb9e211f9c968684764a09c35292026936dbf0
meet <meetbhalodiya1030@gmail.com>
2025-05-12 20:58:05 +0530

    refactor: update sidebar button styling and structure in ResultTable component

--- src/components/claims/ResultTable.tsx
+++ src/components/claims/ResultTable.tsx
@@ -60,38 +60,41 @@ const ResultTable = () => {
 
 	return (
 		<div className="flex h-full w-full relative">
-			{sidebarVisible && (
-				<div className="w-56 absolute top-0 bottom-0 right-0 z-50 border-l border-l-[#D9D9D9] bg-gray-50 py-2 px-4 overflow-y-auto">
-					<div className="font-semibold py-2">Toggle Columns</div>
-					{columnDefs.map((col) => {
-						if (!col.field) return null;
-						return (
-							<div key={col.field} className="flex items-center gap-2 mb-1">
-								<input
-									type="checkbox"
-									id={`col-${col.field}`}
-									checked={!col.hide}
-									onChange={() => toggleColumnVisibility(col.field!)}
-									className="mr-2"
-								/>
-								<label htmlFor={`col-${col.field}`} className="cursor-pointer">
-									{col.headerName || col.field}
-								</label>
-							</div>
-						);
-					})}
-				</div>
-			)}
-
 			<div className="flex flex-row-reverse flex-1">
-				<div className="relative w-8 bg-[#FAFAFA] rounded-tl-md rounded-tr-md">
+				<div className="relative w-8 rounded-tl-md rounded-tr-md">
 					<button
 						onClick={toggleSidebar}
-						className="px-4 h-[33px] bg-gray-200 rotate-90 absolute top-[31px] -left-8 z-50  hover:bg-gray-300 cursor-pointer"
+						className="px-4 h-[33px] rounded-tl-md rounded-tr-md bg-brand-light border border-[#D9D9D9] border-b-0  rotate-90 absolute top-8 -left-8 z-50  hover:bg-brand-light-hover cursor-pointer"
 					>
 						Columns
 					</button>
 				</div>
+
+				{sidebarVisible && (
+					<div className="w-56 border border-[#D9D9D9] border-l-0 bg-brand-light py-2 px-4 overflow-y-auto">
+						<div className="font-semibold py-2">Toggle Columns</div>
+						{columnDefs.map((col) => {
+							if (!col.field) return null;
+							return (
+								<div key={col.field} className="flex items-center gap-2 mb-1">
+									<input
+										type="checkbox"
+										id={`col-${col.field}`}
+										checked={!col.hide}
+										onChange={() => toggleColumnVisibility(col.field!)}
+										className="mr-2"
+									/>
+									<label
+										htmlFor={`col-${col.field}`}
+										className="cursor-pointer"
+									>
+										{col.headerName || col.field}
+									</label>
+								</div>
+							);
+						})}
+					</div>
+				)}
 
 				<div className="flex-1 w-full">
 					<AgGridReact
